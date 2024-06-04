70cec315fdf0fd2ec4546cae7768db589f8fd142
solisut <liowjie229@gmail.com>
2024-06-04 14:15:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: '生日快乐啊',
-    introDesc: `恭喜这位小朋友 又老了一岁
-    but 没有关系 脑没有老就可以了
-    哈哈哈哈哈哈`,
-    btnIntro: '^^哈哈哈^^',
-    title: '那么问题来了，请问你没有完成你的自律？ 🥰',
-    desc: '老实回答 ：D ',
-    btnYes: '没有，我每天睡到早上11点',
-    btnNo: '有，肯定有',
-    question: 'OKAY  第二个问题，请问伟杰有什么优点？',
-    btnReply: '确认',
-    reply: '会钢琴，煮饭，kpop，很多啦，才艺就不讲多。 主要是人长的帅，身上唯一的缺点就是优点太多 <33333333',
-    mess: '嚯 像你这样诚实的人不多了 🥰祝你生日快乐😘😘',
-    messDesc: `今年的生日就没办法陪你过啦。我本来想写信的，但是感觉好像没有创意，又是一样的，你就不要讲什么没有心，懒惰啦🤡
-    erm。。。我想说你谢谢你，我真的很开心遇见你，谢谢你能包容我的性格和脾气，even 我妈妈都顶我不顺🤡
-    还有 你有问过我喜欢你什么，其实我想说的是 不是我喜欢的样子你都有，而是你的样子我都喜欢，所以你就做自己就好啦🤓
-    最后 再祝你生日快乐啦 做大人一年了 不要在这么容易被骗`,
-    btnAccept: '好啦 生日快乐 我爱你😘',
-    messLink: 'https://www.nissanusa.com/vehicles/sports-cars/gt-r.html'
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
