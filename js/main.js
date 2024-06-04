9547b5995854a6c4a07fe426a7f353bf058f77a6
solisut <liowjie229@gmail.com>
2024-06-04 14:03:51 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+    // process bar
+    setTimeout(function() {
+        firstQuestion();
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+
 function init() {
     document.getElementById('titleWeb').innerHTML = CONFIG.titleWeb
     $('#title').text(CONFIG.title)
